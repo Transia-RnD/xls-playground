2513149d97ae4945ef4b89231dbec53e5e451338
Denis Angell <dangell@transia.co>
2023-03-05 07:12:22 +0000

    update test config

--- py/testing_config.py
+++ py/testing_config.py
@@ -9,28 +9,17 @@ from xrpl.clients import WebsocketClient
 # from xrpl.ledger import get_network_id
 from xrpl.wallet import Wallet
 
-from xls_playground.models.tools import fund, deploy_token, fund_ic
-
-masterWallet: Wallet = Wallet("snoPBrXtMeMyMHUVTgbuqAfg1SUTb", 0)
-SEEDS: List[str] = []
-
 
 class BaseTestConfig(TestCase):
     """BaseTestConfig."""
 
     masterSecret = "snoPBrXtMeMyMHUVTgbuqAfg1SUTb"
-    seeds: List[str] = [
-        "sEd7yKm7WVSDkYvpMjvyma7vUiVZiXa",
-        "sEd7b6dwNM1J7fM3aLRZ53JXRqJ99Gf",
-        "sEdTF2b3WePkNbmBBborKEXR9Vk3eBB",
-        "sEdTVG8mVTxNRoCJ7YiYhqmgGCzfTZ1",
-    ]
-
-    masterWallet: Wallet = Wallet(masterSecret, 0)
-    wallets: List[Wallet] = [Wallet(seed, 0) for seed in seeds]
+    wallet: Wallet = Wallet(masterSecret, 0)
 
     BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__)))
     WSS_RPC_URL = "ws://127.0.0.1:6006"
+    LOCK_RPC_URL = "ws://127.0.0.1:6006"
+    ISSUE_RPC_URL = "ws://127.0.0.1:6008"
 
     @classmethod
     def setUpUnit(cls):
@@ -42,35 +31,16 @@ class BaseTestConfig(TestCase):
         cls.client = WebsocketClient(cls.WSS_RPC_URL)
         cls.client.open()
 
+        cls.issue_client = WebsocketClient(cls.LOCK_RPC_URL)
+        cls.issue_client.open()
+
+        cls.lock_client = WebsocketClient(cls.ISSUE_RPC_URL)
+        cls.lock_client.open()
+
     @classmethod
     def setUpIntegration(cls):
         """setUpIntegration."""
         print("SETUP INTEGRATION")
-        w3 = WebsocketClient(cls.WSS_RPC_URL)
-        with w3 as client:
-            # client.network_id = get_network_id(client)
-            currency_code: str = "USD"
-
-            for w in cls.wallets:
-                response = fund(
-                    client,
-                    cls.masterWallet,
-                    w.classic_address,
-                    2000,
-                )
-                # print(response)
-                # if response:
-                #     cls.assertEqual(response.result["engine_result"], "tesSUCCESS")
-
-            # cls.cold_wallet = cls.wallets[0]
-            # cls.hot_wallet = cls.wallets[1]
-            # deploy_token(
-            #     client, currency_code, cls.cold_wallet.seed, cls.hot_wallet.seed
-            # )
-
-            # [fund_ic(client, currency_code, cls.cold_wallet, w) for w in cls.wallets]
-            del cls.wallets[0]
-            del cls.wallets[0]
 
     @classmethod
     def tearDownIntegration(cls):
--- py/xls_playground/tools/__init__.py
+++ py/xls_playground/tools/__init__.py
@@ -2,251 +2,226 @@
 # coding: utf-8
 
 import time
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Union
 
+from xrpl.core.keypairs import generate_seed
 from xrpl.clients import Client, WebsocketClient
 from xrpl.models.requests.ledger_entry import RippleState
 from xrpl.models.amounts.issued_currency_amount import IssuedCurrencyAmount
-from xrpl.models.requests import AccountLines, LedgerEntry, AccountInfo
-from xrpl.models.transactions import Payment, TrustSet, AccountSet, AccountSetFlag
+from xrpl.models.requests import GenericRequest, LedgerEntry, AccountInfo
+from xrpl.models.transactions import (
+    Payment,
+    TrustSet,
+    AccountSet,
+    AccountSetFlag
+)
 from xrpl.transaction import (
+    get_transaction_from_hash,
     safe_sign_and_autofill_transaction,
     submit_transaction,
 )
 from xrpl.utils import xrp_to_drops
 from xrpl.wallet import Wallet
 
+from testing_config import BaseTestConfig
 
-def symbol_to_hex(symbol):
-    """symbol_to_hex."""
-    if len(symbol) > 3:
-        bytes_string = bytes(str(symbol).encode("utf-8"))
-        return bytes_string.hex().upper().ljust(40, "0")
-    return symbol
+LEDGER_ACCEPT_REQUEST = GenericRequest(method="ledger_accept")
 
 
-def get_rs(client: Client, account: str, issuer: str, currency: str) -> LedgerEntry:
-    """get_rs.
+def wait_for_result(client: WebsocketClient, tx_hash: str):
+    """"""
+    timeout: int = 0
+    while timeout <= 8:
+        client.request(LEDGER_ACCEPT_REQUEST)
+        response = get_transaction_from_hash(tx_hash=tx_hash, client=client)
+        if 'validated' in response.result and response.result['validated'] is True:
+            return response
+        time.sleep(1)
+        timeout += 1
+    raise ValueError('test transaction timeout')
 
-    Args:
-        client (Client): client
-        account (str): account
-        issuer (str): issuer
-        currency (str): currency
 
-    Returns:
-        LedgerEntry: ledger entry
-    """
+class Account(object):
+    """"""
+    name: str
+    account: str
+    wallet: Wallet
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+        if name == 'gw':
+            self.wallet = Wallet("sEdSmmFciyvxYaQcdRCv4FhYEJ1aqpn", 0)
+            self.account = self.wallet.classic_address
+        if name == 'alice':
+            self.wallet = Wallet("sEd7mHoS84UWye8epfNvHXkeET1Btfd", 0)
+            self.account = self.wallet.classic_address
+        if name == 'bob':
+            self.wallet = Wallet("sEdTQgHLuZVjcYGoBRV2hvq2iDXUDWZ", 0)
+            self.account = self.wallet.classic_address
+        if name == 'carol':
+            self.wallet = Wallet("sEdSbbjTvsKZ8xNkJSyHVBYPNY3jkR9", 0)
+            self.account = self.wallet.classic_address
+        if name == 'dave':
+            self.wallet = Wallet("sEdSmJx74N6UiDm2uwVzLBmkVuR3HTy", 0)
+            self.account = self.wallet.classic_address
+        if name == 'elsa':
+            self.wallet = Wallet("sEdTeiqmPdUob32gyD6vPUskq1Z7TP3", 0)
+            self.account = self.wallet.classic_address
+
+        if not self.wallet:
+            self.wallet = Wallet.create()
+            self.account = self.wallet.classic_address
+        pass
+
+
+class ICXRP(object):
+    """"""
+    issuer: str
+    currency: str = 'XRP'
+    value: float
+    amount: str
+
+    def __init__(self, value: float) -> None:
+        self.value = value
+        self.amount = xrp_to_drops(value)
+        pass
+
+
+class IC(object):
+    """"""
+    issuer: str
+    currency: str
+    value: float
+    amount: IssuedCurrencyAmount
+
+    @staticmethod
+    def gw(name: str, gw: Account) -> 'IC':
+        self = IC()
+        self.issuer = gw.account
+        self.currency = symbol_to_hex(name)
+        return self
+
+    def __call__(self, value: float) -> Any:
+        self.value = value
+        self.amount = IssuedCurrencyAmount(
+            issuer=self.issuer,
+            currency=self.currency,
+            value=self.value,
+        )
+        return self
+
+    def __init__(self) -> None:
+        pass
+
+
+def xrp_balance(ctx: WebsocketClient, account: Account) -> float:
+    response = ctx.request(
+        AccountInfo(
+            account=account.account,
+        ),
+    )
+    if "error" in response.result and response.result["error"] == "actNotFound":
+        # print(response.result["error"])
+        return 0
+    return float(response.result["account_data"]["Balance"])
+
+
+def ic_balance(ctx: WebsocketClient, account: Account, ic: IC) -> float:
     rs = RippleState(
-        currency=currency,
+        currency=ic.currency,
         accounts=[
-            account,
-            issuer,
+            account.account,
+            ic.issuer,
         ],
     )
-    le = LedgerEntry(ripple_state=rs)
-    return client._request_impl(le)
+    request = LedgerEntry(ripple_state=rs)
+    response = ctx.request(request)
+    if "error" in response.result and response.result["error"] == "entryNotFound":
+        # print(response.result["error"])
+        return 0
+    return float(response.result['node']['Balance']['value'])
 
 
-# # Get LE
-# # [START] Get LE
-# def get_le(client: Client, account: str, issuer: str, currency: str):
-#     """get_le."""
-#     rs = RippleState(
-#         currency=currency,
-#         accounts=[
-#             account,
-#             issuer,
-#         ],
-#     )
-#     le = LedgerEntry(ripple_state=rs)
-#     return client._request_impl(le)
+def balance(ctx: WebsocketClient, account: Account, ic: IC = None) -> float:
+    if not ic:
+        return xrp_balance(ctx, account)
+    return ic_balance(ctx, account, ic)
 
 
-# # [END]Get LE
-
-
-def fund_ic(client: Client, currency_code: str, cold_wallet: Wallet, to_wallet: Wallet):
-    tl_amount: int = 1000000
-    amount: int = 100000
-    balance: float = get_rs(
-        client, to_wallet.classic_address, cold_wallet.classic_address, currency_code
-    )
-    if balance and balance["balance"] >= amount:
-        balance_f: float = balance["balance"]
-        print(
-            f"{to_wallet.classic_address}/{currency_code} ALREADY EXISTS WITH {balance_f} BALANCE"
-        )
-        return True
-
-    # Create trust line from hot to cold address -----------------------------------
-    trust_set_tx = TrustSet(
-        account=to_wallet.classic_address,
-        limit_amount=IssuedCurrencyAmount(
-            currency=symbol_to_hex(currency_code),
-            issuer=cold_wallet.classic_address,
-            value=str(tl_amount),  # Large limit, arbitrarily chosen
-        ),
-    )
-    ts_prepared = safe_sign_and_autofill_transaction(
-        transaction=trust_set_tx,
-        wallet=to_wallet,
-        client=client,
-    )
-    print(
-        f"Creating trust line from {to_wallet.classic_address} to {cold_wallet.classic_address}..."
-    )
-    response = submit_transaction(ts_prepared, client)
-    print(response.result)
-
-    send_token_tx = Payment(
-        account=cold_wallet.classic_address,
-        destination=to_wallet.classic_address,
-        amount=IssuedCurrencyAmount(
-            currency=symbol_to_hex(currency_code),
-            issuer=cold_wallet.classic_address,
-            value=amount,
-        ),
-    )
-    pay_prepared = safe_sign_and_autofill_transaction(
-        transaction=send_token_tx,
-        wallet=cold_wallet,
-        client=client,
-    )
-    print(f"Sending {amount} {currency_code} to {to_wallet.classic_address}...")
-    response = submit_transaction(pay_prepared, client)
-    print(response.result)
-
-
-def fund(client: Client, wallet: Wallet, destination: str, amount: float):
-    drop_value = xrp_to_drops(float(amount))
-    if needs_funding(client, drop_value, destination):
-        send_token_tx = Payment(
+def fund(ctx: WebsocketClient, wallet: Wallet, uicx: Union[IC, ICXRP], *accts: Account):
+    for acct in accts:
+        prepared_tx = Payment(
             account=wallet.classic_address,
-            destination=destination,
-            amount=drop_value,
+            destination=acct.account,
+            amount=uicx.amount,
             # network_id=client.network_id,
         )
-        pay_prepared = safe_sign_and_autofill_transaction(
-            transaction=send_token_tx,
+        signed_tx = safe_sign_and_autofill_transaction(
+            transaction=prepared_tx,
             wallet=wallet,
-            client=client,
+            client=ctx,
         )
-        response = submit_transaction(pay_prepared, client)
-        return response
+        response = submit_transaction(signed_tx, ctx)
+        if "error" in response.result:
+            print(response.result["error"])
+        tx_hash: str = response.result["tx_json"]["hash"]
+        wait_for_result(ctx, tx_hash)
 
 
-def trust_set(client: Client, wallet: Wallet, currency: str, issuer: str, value: float):
-    # Create trust line from hot to cold address -----------------------------------
-    tx = TrustSet(
-        account=wallet.classic_address,
-        limit_amount=IssuedCurrencyAmount(
-            currency=symbol_to_hex(currency),
-            issuer=issuer,
-            value=value,  # Large limit, arbitrarily chosen
-        ),
-    )
-    prepared = safe_sign_and_autofill_transaction(
-        transaction=tx,
-        wallet=wallet,
-        client=client,
-    )
-    response = submit_transaction(prepared, client)
-    print(response.result)
+def pay(ctx: WebsocketClient, uicx: Union[IC, ICXRP], signer: Account, *accts: Account):
+    for acct in accts:
+        prepared_tx = Payment(
+            account=signer.account,
+            destination=acct.account,
+            amount=uicx.amount,
+            # network_id=client.network_id,
+        )
+        signed_tx = safe_sign_and_autofill_transaction(
+            transaction=prepared_tx,
+            wallet=signer.wallet,
+            client=ctx,
+        )
+        response = submit_transaction(signed_tx, ctx)
+        if "error" in response.result:
+            print(response.result["error"])
+        tx_hash: str = response.result["tx_json"]["hash"]
+        wait_for_result(ctx, tx_hash)
 
 
-def deploy_token(
-    client: Client,
-    currency_code: str,
-    cold_wallet: Wallet,
-    hot_wallet: Wallet,
-):
+def trust(ctx: WebsocketClient, uicx: Union[IC, ICXRP], *accts: Account):
+    print(uicx.amount)
+    for acct in accts:
+        prepared_tx = TrustSet(
+            account=acct.account,
+            limit_amount=uicx.amount,
+        )
+        signed_tx = safe_sign_and_autofill_transaction(
+            transaction=prepared_tx,
+            wallet=acct.wallet,
+            client=ctx,
+        )
+        response = submit_transaction(signed_tx, ctx)
+        if "error" in response.result:
+            print(response.result["error"])
+        tx_hash: str = response.result["tx_json"]["hash"]
+        wait_for_result(ctx, tx_hash)
 
-    # Configure issuer (cold address) settings -------------------------------------
-    cold_settings_tx = AccountSet(
-        account=cold_wallet.classic_address,
+
+def account_set(ctx: WebsocketClient, account: Account,):
+    tx = AccountSet(
+        account=account.account,
         transfer_rate=0,
         # tick_size=0,
         domain=bytes.hex("https://usd.transia.io".encode("ASCII")),
         set_flag=AccountSetFlag.ASF_DEFAULT_RIPPLE,
     )
-    cst_prepared = safe_sign_and_autofill_transaction(
-        transaction=cold_settings_tx,
-        wallet=cold_wallet,
-        client=client,
+    signed_tx = safe_sign_and_autofill_transaction(
+        transaction=tx,
+        wallet=account.wallet,
+        client=ctx,
     )
-    print("Sending cold address AccountSet transaction...")
-    response = submit_transaction(cst_prepared, client)
-    print(response.result)
-
-    # Configure hot address settings -----------------------------------------------
-    hot_settings_tx = AccountSet(
-        account=hot_wallet.classic_address,
-        clear_flag=AccountSetFlag.ASF_REQUIRE_AUTH,
-    )
-    hst_prepared = safe_sign_and_autofill_transaction(
-        transaction=hot_settings_tx,
-        wallet=hot_wallet,
-        client=client,
-    )
-    print("Sending hot address AccountSet transaction...")
-    response = submit_transaction(hst_prepared, client)
-    print(response.result)
-
-    # Create trust line from hot to cold address -----------------------------------
-    trust_set_tx = TrustSet(
-        account=hot_wallet.classic_address,
-        limit_amount=IssuedCurrencyAmount(
-            currency=symbol_to_hex(currency_code),
-            issuer=cold_wallet.classic_address,
-            value="10000000000",  # Large limit, arbitrarily chosen
-        ),
-    )
-    ts_prepared = safe_sign_and_autofill_transaction(
-        transaction=trust_set_tx,
-        wallet=hot_wallet,
-        client=client,
-    )
-    print("Creating trust line from hot address to issuer...")
-    response = submit_transaction(ts_prepared, client)
-    print(response.result)
-
-    # Send token -------------------------------------------------------------------
-    issue_quantity = "1000000"
-    send_token_tx = Payment(
-        account=cold_wallet.classic_address,
-        destination=hot_wallet.classic_address,
-        amount=IssuedCurrencyAmount(
-            currency=symbol_to_hex(currency_code),
-            issuer=cold_wallet.classic_address,
-            value=issue_quantity,
-        ),
-    )
-    pay_prepared = safe_sign_and_autofill_transaction(
-        transaction=send_token_tx,
-        wallet=cold_wallet,
-        client=client,
-    )
-    print(
-        f"Sending {issue_quantity} {currency_code} to {hot_wallet.classic_address}..."
-    )
-    response = submit_transaction(pay_prepared, client)
-    print(response.result)
-
-    return response
-
-
-def needs_funding(client: WebsocketClient, exp_balance: int, account: str):
-    result = client.request(
-        AccountInfo(
-            account=account,
-        ),
-    )
-    if "error" in result.result and result.result["error"] == "actNotFound":
-        print("actNotFound")
-        return True
-
-    balance = result.result["account_data"]["Balance"]
-    if int(balance) < int(exp_balance):
-        return True
-    return False
+    response = submit_transaction(signed_tx, ctx)
+    if "error" in response.result:
+        print(response.result["error"])
+    tx_hash: str = response.result["tx_json"]["hash"]
+    wait_for_result(ctx, tx_hash)
