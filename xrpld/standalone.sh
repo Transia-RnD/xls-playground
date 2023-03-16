e6e3537aa18e61c5a4860c79c99aba94259973fe
Denis Angell <dangell@transia.co>
2023-03-16 20:32:24 +0000

    update xrpld standalone

--- xrpld/standalone.sh
+++ xrpld/standalone.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Start rippled in standalone
-exec ./rippled --start --conf config/rippled.cfg -a
+docker compose -f docker-compose.yml up --build --force-recreate -d && exec ./rippled --start --conf config/rippled.cfg -a
