df4141d3c2c625a7b71a163b2724b681b982130f
Denis Angell <dangell@transia.co>
2023-03-27 05:25:45 +0000

    add hook tests

--- ts/test/hooks.test.ts
+++ ts/test/hooks.test.ts
@@ -11,14 +11,15 @@ import {
   calculateHookOn,
   TTS,
   hexNamespace,
+  LedgerEntryRequest,
+  LedgerEntryResponse,
 } from "xrpl";
 import { encode } from "ripple-binary-codec";
+import { TRANSACTION_TYPES } from "ripple-binary-codec";
 
 // INSTALL
 // ----------------------------------------------------------------------------
 // yarn add "https://github.com/Transia-RnD/xrpl.js.git#hooks" --save
-
-const fs = require("fs");
 
 describe("test hooks", function () {
   it("all", async function () {
@@ -37,7 +38,7 @@ describe("test hooks", function () {
       .toUpperCase();
 
     // HookOn
-    const invokeOn: Array<keyof TTS> = ["ttACCOUNT_SET"];
+    const invokeOn: Array<keyof TTS> = ["AccountSet"];
     const hookOn = calculateHookOn(invokeOn);
 
     // NameSpace
@@ -77,5 +78,103 @@ describe("test hooks", function () {
     const response = await client.submit(signedTx);
     assert.equal(response.result.engine_result, "tesSUCCESS");
     await client.disconnect();
+  });
+});
+
+describe("test hooks rpc", function () {
+  it("ledger entry hook_definition", async function () {
+    const WSS_RPC_URL = "wss://hooks-testnet-v3.xrpl-labs.com";
+    const client = new Client(WSS_RPC_URL);
+    await client.connect();
+
+    const hook_hash =
+      "ACD3E29170EB82FFF9F31A067566CD15F3A328F873F34A5D9644519C33D55EB7";
+
+    const ledgerEntryRequest: LedgerEntryRequest = {
+      command: "ledger_entry",
+      hook_definition: hook_hash,
+    };
+
+    const ledgerEntryResponse = await client.request(ledgerEntryRequest);
+    console.log(ledgerEntryResponse);
+
+    const expectedResponse: LedgerEntryResponse = {
+      id: ledgerEntryResponse.id,
+      type: "response",
+      result: {
+        index: ledgerEntryResponse.result.index,
+        ledger_current_index: ledgerEntryResponse.result.ledger_current_index,
+        node: ledgerEntryResponse.result.node,
+        validated: false,
+      },
+    };
+
+    assert.equal(ledgerEntryResponse.type, "response");
+    assert.deepEqual(ledgerEntryResponse, expectedResponse);
+    await client.disconnect();
+  });
+});
+
+function iterateList(arr) {
+  let combinationArray = [] as string[][];
+
+  for (let i = 0; i <= arr.length; i++) {
+    let combination = [] as string[];
+    for (let j = 0; j < i; j++) {
+      combination.push(arr[j] as string);
+    }
+    combinationArray.push(combination as string[]);
+  }
+
+  return combinationArray;
+}
+
+describe("test hooks variables", function () {
+  it("hook on", async function () {
+    const WSS_RPC_URL = "wss://hooks-testnet-v3.xrpl-labs.com";
+    const client = new Client(WSS_RPC_URL);
+    await client.connect();
+    const fullListOfLists = iterateList(TRANSACTION_TYPES);
+    const fList = [] as Record<string, any>[];
+    for (let l = 0; l < fullListOfLists.length; l++) {
+      const list = fullListOfLists[l];
+      const invokeOn: Array<keyof TTS> = list;
+      const hookOn = calculateHookOn(invokeOn);
+      const obj = {
+        list: invokeOn,
+        hookOn: hookOn,
+      };
+      fList.push(obj);
+    }
+    const jsonfString = JSON.stringify(fList);
+    fs.writeFile("./hookonMap.json", jsonfString, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    });
+
+    //   const ffList = {} as Record<string, string[]>;
+    //   for (let t = 0; t < TRANSACTION_TYPES.length; t++) {
+    //     const tt = TRANSACTION_TYPES[t];
+    //     for (let fl = 0; fl < fList.length; fl++) {
+    //       const flo = fList[fl].list;
+    //       if (flo.includes(tt)) {
+    //         if (!ffList[tt]) {
+    //           ffList[tt] = [fList[fl].hookOn];
+    //         } else {
+    //           ffList[tt].push(fList[fl].hookOn);
+    //         }
+    //       }
+    //     }
+    //   }
+    //   const jsonffString = JSON.stringify(ffList);
+    //   fs.writeFile("./hookonByTT.json", jsonffString, (err) => {
+    //     if (err) {
+    //       console.error(err);
+    //       return;
+    //     }
+    //   });
+    //   await client.disconnect();
   });
 });
